b5d42f4355779d9873a5b85525fd06ff0c402165
Temesgen Mesfin Tewolde <codeteme@gmail.com>
2023-05-13 15:33:55 +0300

    basic stats and visualization

--- src/visualization.py
+++ src/visualization.py
@@ -1,16 +1,37 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 qatar_embassies_path = "interim/qatar_embassies_stats.xlsx"
+all_qatar_embassies_path = "interim/all_qatar_embassies_df.xlsx"
+
+# Show a warning message
+st.warning(
+    """This app is still under development. It currently only looks at the Qatari Embassies' tweets.
+    Please use it with caution"""
+)
+
 
 @st.cache_data
 def load_data():
+    """Loads the data from the Excel file."""
     data = pd.read_excel(qatar_embassies_path, engine="openpyxl")
-    return data
+    all_qatar_embassies = pd.read_excel(all_qatar_embassies_path, engine="openpyxl")
+    
+    return data, all_qatar_embassies
+
 
 with st.spinner("Loading data..."):
-    data = load_data()
+    data, all_qatar_embassies = load_data()
     st.success("Data loaded successfully")
+
+
+# Show the raw data
+st.subheader("Country specific data")
+st.write("Expand to view full dataset")
+
+st.write(data)
+
 
 with st.sidebar:
     st.title("Qatar Embassies Stats")
@@ -18,10 +39,23 @@ with st.sidebar:
     country = st.selectbox("Country", data.columns[1:])
     country_data = data.loc[:, country]
 
-st.write(f'Total Tweets: {country_data.iloc[0]}')
-st.write(f'Original Tweets: {country_data.iloc[1]}')
-st.write(f'Retweets: {country_data.iloc[2]}')
-# st.write(f'Unique Dates: {country_data.iloc[3]}')
-st.write(f'Languages: {country_data.iloc[4]}')
-st.write(f'Hashtags: {country_data.iloc[5]}')
 
+st.subheader(f"{country} Stats")
+st.markdown(
+    f"""
+    * Total Tweets: {country_data.iloc[0]}
+    * Original Tweets: {country_data.iloc[1]}
+    * Retweets: {country_data.iloc[2]}
+    * Unique Dates: {country_data.iloc[3]}
+    * Languages: {country_data.iloc[4]}
+    * Hashtags: {country_data.iloc[5]}
+    """
+)
+
+
+# Group by the "screen_name" column
+count_df = all_qatar_embassies.groupby("screen_name").size()
+# Sort the data
+count_df = count_df.sort_values(ascending=False)
+
+st.bar_chart(count_df)
